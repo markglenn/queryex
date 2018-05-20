177d4d4c54b7020ac31f71c3a631d56e3247eedc
Mark Glenn <markglenn@gmail.com>
2018-05-19 23:23:29 -0500

    Rename function
    dense_rank is no longer used, so simplify the name to just include_count

--- lib/query_engine/engine/pager.ex
+++ lib/query_engine/engine/pager.ex
@@ -3,12 +3,12 @@ defmodule QueryEngine.Engine.Pager do
 
   def page(query, limit, offset) do
     query
-    |> dense_rank
+    |> include_count
     |> limit(^(limit || 100))
     |> offset(^(offset || 0))
   end
 
-  defp dense_rank(query) do
+  defp include_count(query) do
     query
     |> select([m: 0], {m, fragment("count(*) OVER() AS __count__")})
   end
